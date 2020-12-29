339ae7a768d9edac97264e6bf5aa7da7d4ec8385
adams43360 <druiz@coffreo.com>
2020-12-29 11:47:51 +0100

    Replace main links to toDefine function

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class HomePage(QDialog):
 
         # Resize the page
         self.resize(1200, 800)
-        self.setWindowTitle("Outil de statistiques pour le Golf Par Damien")
+        self.setWindowTitle("Outil de statistiques pour le Golf")
 
         self.setupUi()
 
@@ -17,22 +17,21 @@ class HomePage(QDialog):
         # Create QMenuBar
         self.header = QMenuBar()
         self.menu_carte = self.header.addMenu("Cartes de score")
-        self.menu_carte.addAction("Enregistrer une cartes", self.connectBdd)
+        self.menu_carte.addAction("Enregistrer une cartes", self.toDefine)
         self.menu_carte.addAction("Mes cartes", self.scoreCards)
         self.menu_stats = self.header.addMenu("Statistiques")
-        self.menu_stats.addAction("Ouvrir mes statistiques", self.connectBdd)
+        self.menu_stats.addAction("Ouvrir mes statistiques", self.toDefine)
 
         # Create layout
         self.layout = QGridLayout()
 
         # Form structuration
-        self.le_name = QLabel('Ici bientôt la page statistiques')
         self.btn_cards = QPushButton('Mes cartes')
         self.btn_stats = QPushButton('Mes Stats')
         self.btn_cards.setMinimumHeight(200)
-        self.btn_cards.clicked.connect(self.connectBdd)
+        self.btn_cards.clicked.connect(self.toDefine)
         self.btn_stats.setMinimumHeight(200)
-        self.btn_stats.clicked.connect(self.connectBdd)
+        self.btn_stats.clicked.connect(self.toDefine)
 
         # Add widgets
         self.layout.addWidget(self.btn_stats, 1, 2, 1, 2)
@@ -45,20 +44,8 @@ class HomePage(QDialog):
         dialog.show()
         dialog.exec_()
 
-    def connectBdd(self):
-        try:
-            conn = sqlite3.connect('golfstats.db')
-            cur = conn.cursor()
-            print("Base de données crée et correctement connectée à SQLite")
-            sql = "SELECT sqlite_version();"
-            cur.execute(sql)
-            res = cur.fetchall()
-            print("La version de SQLite est: ", res)
-            cur.close()
-            conn.close()
-            print("La connexion SQLite est fermée")
-        except sqlite3.Error as error:
-            print("Erreur lors de la connexion à SQLite", error)
+    def toDefine(self):
+        print("Coming soon")
 
 if __name__ == '__main__':
     # Create the Qt Application
