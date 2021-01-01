a2069f3af084132f5c5e1ac1feb1cbfc3adff148
adams43360 <druiz@coffreo.com>
2021-01-01 19:35:10 +0100

    New card modification : Issue on scrollBar and to see how to have several combobox name in the loop

--- cardnew.py
+++ cardnew.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QPushButton, QGridLayout, QDialog, QLabel
+from PyQt5.QtWidgets import QPushButton, QGridLayout, QDialog, QLabel, QScrollBar
 from PyQt5.QtCore import QDate
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sqlite3
@@ -18,6 +18,8 @@ class cardDetail(QDialog):
     def setupUi(self):
         # Create layout
         self.gridLayout = QGridLayout()
+        self.verticalScrollBar = QScrollBar()
+        self.verticalScrollBar.setOrientation(QtCore.Qt.Vertical)
 
         # Form generic structuration
         self.lbl_date = QtWidgets.QLabel("Date de jeu")
@@ -27,6 +29,10 @@ class cardDetail(QDialog):
         self.lbl_course = QtWidgets.QLabel("Parcours")
         self.gridLayout.addWidget(self.lbl_course, 0, 5, 1, 2)
         self.cmb_course = QtWidgets.QComboBox()
+
+        self.cmb_course.currentIndexChanged.connect(self.nbHoles)
+
+
         self.gridLayout.addWidget(self.cmb_course, 0, 7, 1, 3)
         self.lbl_speed = QtWidgets.QLabel("Vitesse de jeu")
         self.gridLayout.addWidget(self.lbl_speed, 1, 0, 1, 2)
@@ -54,31 +60,13 @@ class cardDetail(QDialog):
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.gridLayout.addWidget(self.line, 4, 0, 2, 10)
 
-        # Form with loop for holes to add
-        #  Don't add in a first step, waiting to have the course selected
-
-        self.lbl_hole_1 = QtWidgets.QLabel("Trou 1")
-        self.gridLayout.addWidget(self.lbl_hole_1, 5, 0, 2, 1)
-        self.cmb_hole_1 = QtWidgets.QComboBox()
-        self.gridLayout.addWidget(self.cmb_hole_1, 5, 1, 2, 2)
-        self.lbl_club_1 = QtWidgets.QLabel("Club")
-        self.gridLayout.addWidget(self.lbl_club_1, 5, 3, 2, 1)
-        self.cmb_club_1 = QtWidgets.QComboBox()
-        self.gridLayout.addWidget(self.cmb_club_1, 5, 4, 2, 2)
-        self.chk_farway_1 = QtWidgets.QCheckBox("Fairway")
-        self.gridLayout.addWidget(self.chk_farway_1, 5, 6, 2, 2)
-        self.chk_green_1 = QtWidgets.QCheckBox("Green")
-        self.gridLayout.addWidget(self.chk_green_1, 5, 8, 2, 2)
-        self.txt_comment_1 = QtWidgets.QTextEdit("")
-        self.gridLayout.addWidget(self.txt_comment_1, 7, 0, 1, 10)
-
+        # Insert data in ComboBox
         self.lstCourses()
         self.lstWeather()
         self.lstSpeed()
         self.lstStarter()
         self.lstParty()
         self.lstBalls()
-
 
         # Form generic structuration Footer
         self.btn_valid = QtWidgets.QDialogButtonBox()
@@ -87,7 +75,7 @@ class cardDetail(QDialog):
             QtWidgets.QDialogButtonBox.Close | QtWidgets.QDialogButtonBox.Save)
         self.gridLayout.addWidget(self.btn_valid, 12, 6, 1, 4)
 
-        self.btn_valid.accepted.connect(self.newBdd)
+        self.btn_valid.accepted.connect(self.validForm)
         self.btn_valid.rejected.connect(self.close)
 
         self.setLayout(self.gridLayout)
@@ -104,10 +92,36 @@ class cardDetail(QDialog):
         except sqlite3.Error as error:
             print("Erreur lors de la connexion à SQLite", error)
 
-    def nbHoles(self, course):
-        hole_number = self.bddConnect("SELECT coursesholes FROM courses WHERE courses.idcourses = " + course)
+    def nbHoles(self):
+        hole_number = self.bddConnect("SELECT coursesholes FROM courses WHERE courses.idcourses = " + self.cmb_course.itemData(0))
+        j = 5
 
-        print(hole_number.fetchone())
+        for i in range(hole_number.fetchone()[0]):
+            print(i + 1)
+
+            self.lbl_hole_1 = QtWidgets.QLabel("Trou " + str(i + 1))
+            self.gridLayout.addWidget(self.lbl_hole_1, j, 0, 2, 1)
+            self.cmb_hole_1 = QtWidgets.QComboBox()
+            self.gridLayout.addWidget(self.cmb_hole_1, j, 1, 2, 2)
+            self.lbl_club_1 = QtWidgets.QLabel("Club")
+            self.gridLayout.addWidget(self.lbl_club_1, j, 3, 2, 1)
+            self.cmb_club_1 = QtWidgets.QComboBox()
+            self.gridLayout.addWidget(self.cmb_club_1, j, 4, 2, 2)
+            self.chk_farway_1 = QtWidgets.QCheckBox("Fairway")
+            self.gridLayout.addWidget(self.chk_farway_1, j, 6, 2, 2)
+            self.chk_green_1 = QtWidgets.QCheckBox("Green")
+            self.gridLayout.addWidget(self.chk_green_1, j, 8, 2, 2)
+            self.txt_comment_1 = QtWidgets.QTextEdit("")
+            self.gridLayout.addWidget(self.txt_comment_1, j + 2, 0, 1, 10)
+
+            self.line = QtWidgets.QFrame()
+            self.line.setFrameShape(QtWidgets.QFrame.HLine)
+            self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+            self.gridLayout.addWidget(self.line, j + 3, 0, 2, 10)
+            j = j + 4
+
+        self.gridLayout.addWidget(self.verticalScrollBar, 0, 10, j - 4, 1)
+
 
     def lstCourses(self):
         liste_courses = self.bddConnect("SELECT courses.coursesname, courses.idcourses, golfs.golfsname FROM golfs, courses WHERE courses.idgolfs = golfs.idgolfs ORDER BY golfs.golfsname")
@@ -145,5 +159,5 @@ class cardDetail(QDialog):
         for row in liste_balls:
             self.cmb_balls.addItem(str(row[1]), str(row[0]))
 
-    def newBdd(self):
+    def validForm(self):
         pass
